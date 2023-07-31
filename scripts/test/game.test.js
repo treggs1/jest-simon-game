63d593acda494d781ac6609bc23e929eed9711cd
Treggs1 <treggs1@gmail.com>
2023-07-31 21:36:42 +0100

    Update tests

--- scripts/test/game.test.js
+++ scripts/test/game.test.js
@@ -1,3 +1,25 @@
 /**
  * @jest-environment jsdom
  */
+
+// npm install --save-dev jest@26.6.3
+
+const { game } = require("../game");
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+});
+
+test("currentGame key exists", () => {
+    expect("currentGame" in game).toBe(true);
+});
+});
