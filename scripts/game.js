6f7560276824d98d400db77ec72ab4f70f9494cb
Treggs1 <treggs1@gmail.com>
2023-08-01 20:59:16 +0100

    Add new tests and functions

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,19 @@
 let game = {
     score: 0,
     currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
 };
 
-module.exports = { game };
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+    showScore();
+}
+
+function showScore() {
+    document.getElementById("score").innertext = game.score;
+}
+
+module.exports = { game, newGame, showScore };
--- scripts/test/game.test.js
+++ scripts/test/game.test.js
@@ -4,7 +4,7 @@
 
 // npm install --save-dev jest@26.6.3
 
-const { game } = require("../game");
+const { game, newGame, showScore } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -15,6 +15,7 @@ beforeAll(() => {
 });
 
 describe("game object contains correct keys", () => {
+    
     test("score key exists", () => {
         expect("score" in game).toBe(true);
 });
@@ -22,4 +23,43 @@ describe("game object contains correct keys", () => {
 test("currentGame key exists", () => {
     expect("currentGame" in game).toBe(true);
 });
+
+test("playerMoves key exists", () => {
+    expect("playerMoves" in game).toBe(true);
+});
+
+test("choices key exists", () => {
+    expect("choices" in game).toBe(true);
+});
+
+test("choices contain correct ids", () => {
+    expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+});
+
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = [1, 2, 3];
+        game.currentGame = [4, 5, 6];
+        document.getElementById("score").innerText = "42";
+        newGame();
+        showScore();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+
+    test("should clear playerMoves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+
+    test("should clear currentGame array", () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
+    });
 });
